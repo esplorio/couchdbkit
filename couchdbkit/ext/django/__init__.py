48b47cd311b15e6b7277e387b01a9b7f80f2ee2a
Son Pham <me@famanson.com>
2018-07-04 09:41:53 +0100

    Replace deprecated method

--- couchdbkit/ext/django/__init__.py
+++ couchdbkit/ext/django/__init__.py
@@ -91,9 +91,11 @@ It won't destroy your datas, just synchronize views.
 
 from django.db.models import signals
 
+
 def syncdb(app, created_models, verbosity=2, **kwargs):
     """ function used by syncdb signal """
     from couchdbkit.ext.django.loading import couchdbkit_handler
     couchdbkit_handler.sync(app, verbosity=verbosity)
 
-signals.post_syncdb.connect(syncdb)
+
+signals.post_migrate.connect(syncdb)
